903ca340e62d23b475e11963a3590df69f2cb8d1
kronosmichall <kronosmichall@gmail.com>
2024-04-11 13:38:39 +0200

    compose.yml

--- 01-docker-terraform/docker/pipeline.sh
+++ 01-docker-terraform/docker/pipeline.sh
@@ -1,4 +1,5 @@
-URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-09.csv.gz"
+DATA_URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-09.csv.gz"
+TIMES_URL="https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv"
 
 docker run -it \
     --network pg-network \
@@ -6,5 +7,14 @@ docker run -it \
         --port 5432 \
         --db postgres \
         --table green_taxi_data \
-        --url ${URL} \
+        --url ${DATA_URL} \
+        --host pg-database
+
+docker run -it \
+    --network pg-network \
+    taxi_ingest:v1 \
+        --port 5432 \
+        --db postgres \
+        --table green_taxi_times \
+        --url ${TIMES_URL} \
         --host pg-database
